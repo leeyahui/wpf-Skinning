94f577913ae5296a0ea06762b128ad04cc5c4680
leeyahui <liyahui900508@163.com>
2020-03-19 17:41:36 +0800

    添加项目文件。

---
+++ DemoWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+using Skinning.Util;
+
+namespace Skinning
+{
+    /// <summary>
+    /// Interaction logic for DemoWindow.xaml
+    /// </summary>
+    public partial class DemoWindow : Window
+    {
+        public DemoWindow()
+        {
+            InitializeComponent();
+            ResourceHelper.LoadResource("pack://siteOfOrigin:,,,/Modes/GroupMode.xaml");
+        }
+
+        private void OnListViewClick(object sender, RoutedEventArgs e)
+        {
+            ResourceHelper.LoadResource("pack://siteOfOrigin:,,,/Modes/ListMode.xaml");
+        }
+
+        private void OnGroupViewClick(object sender, RoutedEventArgs e)
+        {
+            ResourceHelper.LoadResource("pack://siteOfOrigin:,,,/Modes/GroupMode.xaml");
+        }
+
+        private void OnTreeViewClick(object sender, RoutedEventArgs e)
+        {
+            ResourceHelper.LoadResource("pack://siteOfOrigin:,,,/Modes/TreeMode.xaml");
+        }
+    }
+}
---
+++ Util/ResourceHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace Skinning.Util
+{
+    static class ResourceHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        public static void LoadResource(string fileName)
+        {
+            Application.Current.Resources.MergedDictionaries[0] = new ResourceDictionary()
+            {
+                Source = new Uri(fileName, UriKind.RelativeOrAbsolute)
+            };
+        }
+    }
+}
